daf404feed86f2d0f595b4603b7f42a3b0e6b675
Monolit2000 <kostuchenkojr@gmail.com>
2024-04-19 01:18:41 +0300

    Add CommonModule

---
+++ Common/OrderDeliverySystem.CommonModule.Infrastructure/EventBus/IntegrationEvent.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderDeliverySystem.CommonModule.Infrastructure.EventBus
+{
+    public abstract class IntegrationEvent : INotification
+    {
+        public Guid Id { get; }
+
+        public DateTime OccurredOn { get; }
+
+        protected IntegrationEvent(Guid id, DateTime occurredOn)
+        {
+            this.Id = id;
+            this.OccurredOn = occurredOn;
+        }
+    }
+}
