03c6c49cff5c3269d5af733136ed754357d01ce5
Monolit2000 <46726840+Monolit2000@users.noreply.github.com>
2024-06-09 01:01:01 +0300

    Added ChangeOrderDeliveryTime

---
+++ Modules/Ordering/OrderDeliverySystem.Ordering.Application/Orders/ChangeOrderDeliveryTime/ChangeOrderDeliveryTimeCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using FluentResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderDeliverySystem.Ordering.Application.Orders.ChangeOrderDeliveryTime
+{
+    public class ChangeOrderDeliveryTimeCommandHandler : IRequestHandler<ChangeOrderDeliveryTimeCommand, Result<ChangeOrderDeliveryTimeResult>>
+    {
+        public Task<Result<ChangeOrderDeliveryTimeResult>> Handle(ChangeOrderDeliveryTimeCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
